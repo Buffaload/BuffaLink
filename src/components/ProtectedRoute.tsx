c2561172a426dc6e0e586d8c8a0fac8656eefcef
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2024-11-12 12:23:39 +0000

    update

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 
 interface ProtectedRouteProps {
-  children: React.ReactNode;
+  children: ReactNode;
   token: string | null;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, token }) => {
-  return token ? children : <Navigate to="/login" />;
+  return token ? <>{children}</> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
