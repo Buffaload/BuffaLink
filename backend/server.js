8159b59bfa0efc4d7e507fbd08a8dafdd86f345e
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2024-11-13 10:55:30 +0000

    mongoose connection updates

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,12 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected..."))
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 //Test route
