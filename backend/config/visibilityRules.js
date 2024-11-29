531512b3e05dfde8e6641ffc82d7d6ae10c73b4a
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2024-11-29 23:29:54 +0000

    edited visibility groups

--- backend/config/visibilityRules.js
+++ backend/config/visibilityRules.js
@@ -1,4 +1,25 @@
+// Common collections
+const commonGroups = {
+  avonmouth: ["Avonmouth DVS Units", "Avonmouth Non-DVS"],
+  trailers: [
+    "42 Pallet Deckers",
+    "Baymasters",
+    "Big Door 44s",
+    "Small Door 44s",
+  ],
+};
+
 export const depotVisibilityRules = {
-  ellington: ["Ellington DVS Units"], // Units visibile to Ellington depot account
-  crewe: ["Crewe DVS Units"], // Units visibile to Crewe depot account
+  ellington: [
+    "Ellington DVS Units",
+    "Ellington Non-DVS",
+    ...commonGroups.trailers,
+    ...commonGroups.avonmouth,
+  ], // Units visibile to Ellington depot account
+  crewe: [
+    "CRSK DVS Units",
+    "CRSK Non-DVS",
+    ...commonGroups.trailers,
+    ...commonGroups.avonmouth,
+  ], // Units visibile to Crewe depot account
 };
