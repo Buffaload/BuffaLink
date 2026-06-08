9dc89cd26a56a61d4c55615ca2cd9bb9044e1a60
Jake Leonce <jake.leonce@buffaload.co.uk>
2026-06-08 12:42:49 +0100

    fix(client): build error

--- client/src/components/Vehicles.tsx
+++ client/src/components/Vehicles.tsx
@@ -2095,6 +2095,8 @@ const Vehicles: React.FC<VehiclesProps> = ({
               <div className="vehicle-modal-left">
                 <div className="vehicle-modal-section">
                   <div className="vehicle-modal-section-title">Vehicle details</div>
+
+                  <div className="vehicle-modal-details">
                     <div className="vehicle-modal-detail-row">
                       <span className="vehicle-modal-detail-label">VIN</span>
                       <span className="vehicle-modal-detail-value">
